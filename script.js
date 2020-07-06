3d5b8303999d649418ac4d53596f3ebd68dc923b
tylermcleant <tylermcleant@aol.com>
2020-07-06 18:18:02 -0400

    added prompts

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var uselength = confirm ("Do you want to use length Criteria?");
+  var charctrcrit= confirm (" Do you want any character criteria in your password?");
+  if (charctrcrit==true){
+  var lower= confirm ("Do you require using lowercase characters?")
+  var upper= confirm("Do you require using uppercase characters?")
+  var numeric= confirm("Do you require numbers in your password?")
+  var special= confirm("Do you want special characters in your password?") 
+  }
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
