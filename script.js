ecb70dd7dd25c481cee7191947e3b83c4d326c79
tylermcleant <tylermcleant@aol.com>
2020-07-08 16:44:27 -0400

    completed password generator

--- script.js
+++ script.js
@@ -4,15 +4,40 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var uselength = confirm ("Do you want to use length Criteria?");
-  var charctrcrit= confirm (" Do you want any character criteria in your password?");
-  if (charctrcrit==true){
-  var lower= confirm ("Do you require using lowercase characters?")
-  var upper= confirm("Do you require using uppercase characters?")
-  var numeric= confirm("Do you require numbers in your password?")
-  var special= confirm("Do you want special characters in your password?") 
+  if (uselength==true){ 
+    var length = prompt("How long would you like your password?");
   }
+  else {
+    var length = 8;
+  }
+  var charctrcrit= confirm (" Do you want any character criteria in your password?");
+  var characters=''
+  if (charctrcrit==true){
+    var lower= confirm ("Do you require using lowercase characters?")
+  if (lower==true){
+    characters+='abcdefghijklmnopqrstuvwxyz'
+  }
+    var upper= confirm("Do you require using uppercase characters?")
+  if (upper==true){
+    characters+='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  }
+    var numeric= confirm("Do you require numbers in your password?")
+  if (numeric==true){
+    characters+='0123456789'
+  }
+    var special= confirm("Do you want special characters in your password?") 
+  if (special==true){
+    characters+='~@#$%^&*?-!'
+  }
+  }
+    var result           = '';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
   
-  var password = generatePassword();
+  var password = result;
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
